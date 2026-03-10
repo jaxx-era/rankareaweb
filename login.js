349699c14f50df7e0e23de434685b613dfaedda9
jaxx-era <koenn.jhon@gmail.com>
2026-03-10 23:55:14 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,20 @@
 export default function handler(req, res) {
 
-if (req.method !== "POST") {
-return res.status(405).json({ message: "Method not allowed" });
+if (req.method === "GET") {
+return res.status(200).json({
+message: "Login API working"
+});
 }
+
+if (req.method === "POST") {
 
 const { email, password } = req.body;
 
-if (!email || !password) {
-return res.status(400).json({ message: "Email and password required" });
-}
-
 return res.status(200).json({
-message: "Login API working",
+message: "Login success",
 email: email
 });
 
-  }
+}
+
+}
